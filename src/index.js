6c96b98742ceb6bbdff6831595fb871be871a4e0
DEV ARTSID <you@example.com>
2024-07-18 13:55:47 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,24 +1,32 @@
 import './styles.css'
 
+// Импорт изображений
+import americanImage from './images/american.png'
+import dinersImage from './images/diners.png'
+import discoverImage from './images/discover.png'
+import maestroImage from './images/maestro.png'
+import mirImage from './images/mir.png'
+import visaImage from './images/visa.png'
+import webmoneyImage from './images/webmoney.png'
+
 const cardNumberInput = document.getElementById('cardNumberInput')
 const validateButton = document.getElementById('validateButton')
 const cardImages = document.querySelector('.card-images')
 const validationResult = document.getElementById('validationResult')
 
 const cardTypes = [
-	{ name: 'visa', pattern: /^4/ },
-	{ name: 'maestro', pattern: /^5[1-5]/ },
-	{ name: 'american', pattern: /^3[47]/ },
-	{ name: 'discover', pattern: /^6(?:011|5)/ },
-	{ name: 'diners', pattern: /^3(?:0[0-5]|[68])/ },
-	{ name: 'webmoney', pattern: /^(?:2131|1800|35)/ },
-	{ name: 'mir', pattern: /^220[0-4]/ },
+	{ name: 'visa', pattern: /^4/, image: visaImage },
+	{ name: 'maestro', pattern: /^5[1-5]/, image: maestroImage },
+	{ name: 'american', pattern: /^3[47]/, image: americanImage },
+	{ name: 'discover', pattern: /^6(?:011|5)/, image: discoverImage },
+	{ name: 'diners', pattern: /^3(?:0[0-5]|[68])/, image: dinersImage },
+	{ name: 'webmoney', pattern: /^(?:2131|1800|35)/, image: webmoneyImage },
+	{ name: 'mir', pattern: /^220[0-4]/, image: mirImage },
 ].slice(0, 7)
 
-// Создаем элементы карт и добавляем их в DOM
 cardTypes.forEach(card => {
 	const img = document.createElement('img')
-	img.src = `./images/${card.name.toLowerCase()}.png`
+	img.src = card.image
 	img.alt = card.name
 	cardImages.appendChild(img)
 })
@@ -40,11 +48,26 @@ function validateCardNumber(cardNumber) {
 	if (!regex.test(cardNumber)) return false
 
 	const cardType = getCardType(cardNumber)
-	return !!cardType
+	return !!cardType && luhnCheck(cardNumber)
 }
 
 function getCardType(cardNumber) {
 	return cardTypes.find(card => card.pattern.test(cardNumber))
+}
+
+function luhnCheck(cardNumber) {
+	let sum = 0
+	let shouldDouble = false
+	for (let i = cardNumber.length - 1; i >= 0; i--) {
+		let digit = parseInt(cardNumber[i], 10)
+		if (shouldDouble) {
+			digit *= 2
+			if (digit > 9) digit -= 9
+		}
+		sum += digit
+		shouldDouble = !shouldDouble
+	}
+	return sum % 10 === 0
 }
 
 function showValidationResult(message, color) {
--- webpack.common.js
+++ webpack.common.js
@@ -1,23 +1,25 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
 	entry: './src/index.js',
 	output: {
-		filename: 'bundle.js',
+		filename: 'index.js',
 		path: path.resolve(__dirname, 'dist'),
+		clean: true,
 	},
 	module: {
 		rules: [
 			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
+				test: /\.css$/i,
+				use: [MiniCssExtractPlugin.loader, 'css-loader'],
 			},
 			{
 				test: /\.(png|svg|jpg|jpeg|gif)$/i,
 				type: 'asset/resource',
 				generator: {
-					filename: 'images/[name][ext]',
+					filename: 'images/[name][hash][ext][query]',
 				},
 			},
 		],
@@ -26,5 +28,6 @@ module.exports = {
 		new HtmlWebpackPlugin({
 			template: './src/index.html',
 		}),
+		new MiniCssExtractPlugin(),
 	],
 }
